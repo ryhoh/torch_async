268cde695d968804115aa2a5468769972bfa4d5b
ryhoh <axis38akasira@gmail.com>
2020-10-08 16:34:39 +0900

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,5 +2,5 @@ GPU_IDX=0
 
 for seed in $(seq 0 9)
 do
-  python3 /procedure/learn.py -seed "$seed" -gpu "$GPU_IDX" 2>&1 | tee res"${seed}".txt
+  python3 procedure/learn.py -seed "$seed" -gpu "$GPU_IDX" 2>&1 | tee res"${seed}".txt
 done
