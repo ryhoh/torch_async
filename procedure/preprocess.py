9adad4fa3fecd7b7cf7d6321ca72e74b79350f60
ryhoh <axis38akasira@gmail.com>
2021-07-06 02:05:47 +0900

    Update preprocess.py

--- procedure/preprocess.py
+++ procedure/preprocess.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-from os import path
+from os import environ, path
 from PIL import Image
 from pycocotools.mask import frPyObjects, decode
 from torch.utils.data import DataLoader
@@ -141,11 +141,11 @@ def CocoDetection_loaders() -> Tuple[DataLoader, DataLoader]:
     # ])
 
     # data_sets
-    train_set = CocoDetection(root=path.join('~/dataset/', 'coco/train2017'),
-                              annFile=path.join('~/dataset/', 'coco/annotations/instances_train2017.json',))
+    train_set = CocoDetection(root=path.join(environ['HOME'], 'coco/train2017'),
+                              annFile=path.join(environ['HOME'], 'coco/annotations/instances_train2017.json',))
                               # transform=transform)
-    val_set = CocoDetection(root=path.join('~/dataset/', 'coco/val2017'),
-                            annFile=path.join('~/dataset/', 'coco/annotations/instances_val2017.json',))
+    val_set = CocoDetection(root=path.join(environ['HOME'], 'coco/val2017'),
+                            annFile=path.join(environ['HOME'], 'coco/annotations/instances_val2017.json',))
                             # transform=transform)
 
     # data_loader
