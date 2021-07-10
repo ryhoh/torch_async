e8f4e7d8feb66bd48a533f9ac4a815581b479a69
ryhoh <axis38akasira@gmail.com>
2021-07-10 22:25:38 +0900

    conduct expr
    ViT + CIFAR100 (拡張版RU)

--- runner.sh
+++ runner.sh
@@ -4,9 +4,9 @@ export PYTHONPATH="$PYTHONPATH:~/torch_async/models"
 export PYTHONPATH="$PYTHONPATH:~/torch_async/preprocess"
 
 GPU_IDX=0
-EPOCHS=10
+EPOCHS=15
 
-for seed in $(seq 0 0)
+for seed in $(seq 0 3)
 do
   python3 procedure/learn.py --seed "$seed" --gpu "$GPU_IDX" --epochs "$EPOCHS" 2>&1 | tee res"${seed}".txt
 done
