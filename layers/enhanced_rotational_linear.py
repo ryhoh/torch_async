89a59bd3ab9bdd0bfc0b49b7ce95f2ebe16d5581
ryhoh <axis38akasira@gmail.com>
2021-07-10 10:50:28 +0900

    update RotationalLinear
    Transformer 内で 3D Tensor のBackprop ができていないというエラーだったので，それらしい対策をする

---
+++ layers/enhanced_rotational_linear.py
@@ -0,0 +1,87 @@
+from rotational_update import RotationalLinear
+import torch
+from torch import Tensor
+from torch.autograd import Function
+from torch.nn.functional import linear
+
+
+class BackwardError(Exception):
+    pass
+
+
+class EnhancedRotationalLinearFunction(Function):
+    @staticmethod
+    def forward(ctx, *args) -> Tensor:
+        x, w, b, learn_left, learn_right = args
+        learn_l = torch.as_tensor(learn_left).requires_grad_(False)
+        learn_r = torch.as_tensor(learn_right).requires_grad_(False)
+
+        ctx.save_for_backward(x, w, b, learn_l, learn_r)
+        return linear(x, w, b)
+
+    @staticmethod
+    def backward(ctx, *args: Tensor) -> tuple:
+        grad = args[0]
+        x, w, b, learn_l, learn_r = ctx.saved_tensors
+        w = w.t()
+        learn_l, learn_r = int(learn_l), int(learn_r)
+
+        # バイアスへの勾配は、0ベクトルを作って必要な要素だけ値を入れる
+        # gradients for bias, make 0 vector and insert value into needed element
+        if grad.is_cuda:
+            d_b = torch.zeros(size=(grad.shape[1],), dtype=torch.float32, device='cuda')
+        else:
+            d_b = torch.zeros(size=(grad.shape[1],), dtype=torch.float32)
+        d_b[learn_l:learn_r] = torch.sum(grad[:, learn_l:learn_r], dim=0)
+
+        # 重みへの勾配は、0行列を作って必要な行だけ値を入れる
+        # gradients for weights, make 0 matrix and insert value into needed column
+        if grad.is_cuda:
+            d_w = torch.zeros(size=(x.shape[1], grad.shape[1]), dtype=torch.float32, device='cuda')
+        else:
+            d_w = torch.zeros(size=(x.shape[1], grad.shape[1]), dtype=torch.float32)
+
+        if x.ndim == 2:  # Primitive Backward
+            d_w[:, learn_l:learn_r] = torch.matmul(x.t(), grad[:, learn_l:learn_r])
+        elif x.ndim == 3:  # 3D Backward (not sure...)
+            d_w[:, learn_l:learn_r] = torch.matmul(x.permute(0, 2, 1), grad[:, learn_l:learn_r])
+        else:
+            raise BackwardError("x.ndim == %d" % x.ndim)
+
+        if w.ndim == 2:  # Primitive Backward
+            d_x = torch.matmul(grad, torch.t(w))
+        elif w.ndim == 3:  # 3D Backward (not sure...)
+            d_x = torch.matmul(grad, w.permute(0, 2, 1))
+        else:
+            raise BackwardError("w.ndim == %d" % w.ndim)
+
+        if d_w.ndim == 2:  # Primitive Backward
+            return d_x, d_w.t(), d_b, None, None
+        elif w.ndim == 3:  # 3D Backward (not sure...)
+            return d_x, d_w.permute(0, 2, 1), d_b, None, None
+        else:
+            raise BackwardError("d_w.ndim == %d" % d_w.ndim)
+
+
+class EnhancedRotationalLinear(RotationalLinear):
+    def forward(self, input_tensor) -> Tensor:
+        """
+        Feed-forward method
+        Almost same to normal Linear object.
+        Save variables for learning group.
+
+        Parameters
+        ----------
+        input_tensor : torch.Tensor
+
+        Returns
+        -------
+        res : Output of feed-forwarding.
+        """
+        self.learn_l = self.group_partition[self.group_i - 1]
+        self.learn_r = self.group_partition[self.group_i]
+
+        matmul = EnhancedRotationalLinearFunction.apply
+        res = matmul(input_tensor, self.weight, self.bias, self.learn_l, self.learn_r)
+
+        return res
--- models/rotatedViT.py
+++ models/rotatedViT.py
@@ -1,15 +1,17 @@
 from pytorch_pretrained_vit import ViT
 from rotational_update import RotationalLinear
 
+from layers.enhanced_rotational_linear import EnhancedRotationalLinear
+
 
 def apply_rotational_into_ViT_Projections(model: ViT) -> ViT:
     blocks = model.transformer.blocks
     for i in range(len(blocks)):
         attn = blocks[i].attn
-        attn.proj_q = RotationalLinear(attn.proj_q, reduce_backward=False)
-        attn.proj_k = RotationalLinear(attn.proj_k, reduce_backward=False)
-        attn.proj_v = RotationalLinear(attn.proj_v, reduce_backward=False)
-        blocks[i].proj = RotationalLinear(blocks[i].proj, reduce_backward=False)
+        attn.proj_q = EnhancedRotationalLinear(attn.proj_q, reduce_backward=False)
+        attn.proj_k = EnhancedRotationalLinear(attn.proj_k, reduce_backward=False)
+        attn.proj_v = EnhancedRotationalLinear(attn.proj_v, reduce_backward=False)
+        blocks[i].proj = EnhancedRotationalLinear(blocks[i].proj, reduce_backward=False)
     return model
 
 
@@ -17,6 +19,6 @@ def apply_rotational_into_ViT_PositionWiseFeedForward(model: ViT) -> ViT:
     blocks = model.transformer.blocks
     for i in range(len(blocks)):
         pwff = blocks[i].pwff
-        pwff.fc1 = RotationalLinear(pwff.fc1, reduce_backward=False)
-        pwff.fc2 = RotationalLinear(pwff.fc2, reduce_backward=False)
+        pwff.fc1 = EnhancedRotationalLinear(pwff.fc1, reduce_backward=False)
+        pwff.fc2 = EnhancedRotationalLinear(pwff.fc2, reduce_backward=False)
     return model
